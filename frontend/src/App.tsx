3226596f61db60dd835b132c3393a45b2866b31a
caden c <cadenc.woss@gmail.com>
2024-12-31 15:27:34 -0500

    fix ga

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'; 
 import Dashboard from './pages/Dashboard'; 
+import ReactGA from "react-ga4";
 
 
 const App: React.FC = () => {
+  useEffect(() => {
+    ReactGA.send("pageview");
+  }, []);
   return (
     <Router>
       <Routes>
