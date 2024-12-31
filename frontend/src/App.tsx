bdfab3c589e6d594e78224b2199b7d27f4877234
caden c <cadenc.woss@gmail.com>
2024-12-31 15:10:29 -0500

    add google ga

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'; 
 import Dashboard from './pages/Dashboard'; 
+import ReactGA from 'react-ga';
 
 
 const App: React.FC = () => {
+  useEffect(() => {
+    ReactGA.initialize('G-0NHX9CKWE0');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Router>
       <Routes>
